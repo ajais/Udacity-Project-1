35fe3be618acafec6e8f6db51c9483748ceece26
Alexandre Jais <ajais@fb.com>
2017-06-10 18:40:51 -0700

    Changed style, better movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,35 +2,35 @@ import media
 import fresh_tomatoes
 
 # Generate the movie objects by calling the constructor media.Movie()
-toy_story = media.Movie("Toy Story",
-                        "A Story of a boy and his toys that come to life",
-                        "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie("Moana",
+                        "A little girl with a big mission",
+                        "http://www.impawards.com/2016/posters/moana_ver6.jpg",
+                        "https://www.youtube.com/watch?v=LKFuXETZUsI")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://www.impawards.com/2009/posters/avatar_xlg.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+avatar = media.Movie("Le Premier Jour...",
+                     "The life of a French family",
+                     "http://medias.unifrance.org/medias/28/43/76572/format_page/media.jpg",
+                     "https://www.youtube.com/watch?v=42qLwz1lVNw")
 
-ratatouille = media.Movie("Ratatouille",
-                     "A mouse becomes a chef",
-                     "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                     "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+ratatouille = media.Movie("La La Land",
+                     "A whimsical tale of two artists and lovers",
+                     "https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png",
+                     "ttps://www.youtube.com/watch?v=0pdqf4P9MB8")
 
-school_of_rock = media.Movie("School of Rock",
-                     "Kids learn life through rock and roll music",
-                     "http://img.moviepostershop.com/the-school-of-rock-movie-poster-2003-1020191888.jpg",
-                     "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+school_of_rock = media.Movie("Apocalypse Now",
+                     "Diving into the horrors of the vietnam war",
+                     "http://www.impawards.com/1979/posters/apocalypse_now_ver2_xlg.jpg",
+                     "https://www.youtube.com/watch?v=IkrhkUeDCdQ")
 
-midnight_in_paris = media.Movie("Midnight in Paris",
-                     "A 21st century man wanders magically in 1920 Paris",
-                     "https://images-na.ssl-images-amazon.com/images/I/61uuYEUFw4L._SY450_.jpg",
-                     "https://www.youtube.com/watch?v=FAfR8omt-CY")
+midnight_in_paris = media.Movie("Whatever Works",
+                     "Larry David gets confused.",
+                     "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NTA4NzgyNl5BMl5BanBnXkFtZTcwNzEzMjQ1Mg@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                     "https://www.youtube.com/watch?v=7vvDhtfil3U")
 
-hunger_games = media.Movie("Hunger Games",
-                     "A marine on an alien planet",
-                     "https://images-na.ssl-images-amazon.com/images/I/51OGv-AnD6L.jpg",
-                     "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+hunger_games = media.Movie("Harry Potter and the Prisoner of Azkaban",
+                     "Harry gets older and the danger greater",
+                     "http://www.gstatic.com/tv/thumb/movieposters/34483/p34483_p_v8_ap.jpg",
+                     "https://www.youtube.com/watch?v=lAxgztbYDbs")
 
 # Create a data structure with our movies
 movies=[toy_story,avatar,ratatouille,school_of_rock,midnight_in_paris,hunger_games]
